f52f90361ab3abfd379c0f501c0dc0980e84bcdc
Jack - <jackmerry123@hotmail.com>
2021-06-15 17:54:01 +0100

    db connection established

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+require('dotenv').config('./.env');
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000; 
+const db = require('./dbConnection.js')
 
+
+app.get('/test', async (req, res) => {
+    console.log('/test running');
+    try{
+        const result = await db.query('SELECT * FROM users');
+        res.json(result.rows);
+
+    }catch(err){
+        return res.status(500).send('Internal server error');
+    }
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-})
+});
+
---
+++ dbConnection.js
@@ -0,0 +1,23 @@
+
+const {Pool, client} = require('pg');
+ 
+// pools will use environment variables
+// for connection information
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+});
+
+
+module.exports = {
+    async query(text, params) {
+      const start = Date.now()
+      const res = await pool.query(text, params)
+      const duration = Date.now() - start
+      console.log('executed query', { text, duration, rows: res.rowCount })
+      return res
+    },
+}
