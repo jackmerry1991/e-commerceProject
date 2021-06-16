eba14146adbc66ec887859fac1b758bb2d1290b0
Jack - <jackmerry123@hotmail.com>
2021-06-16 18:27:58 +0100

    get all users and get single user api routes added

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const app = express();
 const port = 3000; 
 const db = require('./dbConnection.js')
 
+//user routes move to users.js after this initial test
 
-app.get('/test', async (req, res) => {
-    console.log('/test running');
+/**
+ * Return all users
+ */
+app.get('/users', async (req, res) => {
+    console.log('/user running');
     try{
         const result = await db.query('SELECT * FROM users');
         res.json(result.rows);
@@ -16,6 +20,24 @@ app.get('/test', async (req, res) => {
         return res.status(500).send('Internal server error');
     }
 });
+
+/**
+ * Return single user
+ */
+app.get('/user/:id', async (req, res) => {
+    console.log('/get-user running');
+    const userId = req.params.id;
+    console.log(req.params.id);
+    try{
+        const result = await db.query(`SELECT * FROM users WHERE user_id = $1`, [userId]);
+        if(result.rows.length < 1) return res.status(200).send('No mathcing users found');
+        res.json(result.rows);
+    }catch(err){
+        return res.status(500).send('Error retrieving results from database');
+    };
+ 
+});
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
