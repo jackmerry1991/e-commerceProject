4e1b65914c8b56ded9468b16c4445c6b090efefd
Jack - <jackmerry123@hotmail.com>
2021-06-23 19:02:55 +0100

    delete user added

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 const db = require('./dbConnection.js')
-
+const bodyParser = require('body-parser');
 //user routes move to users.js after this initial test
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 /**
  * Return all users
@@ -35,9 +41,59 @@ app.get('/user/:id', async (req, res) => {
     }catch(err){
         return res.status(500).send('Error retrieving results from database');
     };
- 
 });
 
+/**
+ * Create new User
+ */
+app.post('/user/create', async (req, res, next) => {
+    
+    const userDetails = {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        street: street,
+        city: city,
+        password: password,
+        postCode: postCode,
+        } = req.body;
+    try{
+        const result = await db.query('INSERT INTO USERS(email, first_name, last_name, street, city, password, post_code) VALUES($1, $2, $3, $4, $5, $6, $7);', [email, firstName, lastName, street, city, password, postCode]);
+        res.send(`User ${email} added successfully`);
+    
+    }catch(err){
+        res.status(500).send('Error updating DB. Internal Server Error.');
+        console.log(err);
+    }   
+});
+
+/**
+ * Update existing user
+ */
+
+/**
+ * Delete existing user
+ */
+app.delete('/user/delete', async (req, res) => {
+    const userId = req.body.userId;
+
+    if(!userId) return res.status(400).send('Insufficent data provided.');
+
+    try{
+        const user = await db.query('SELECT * FROM users WHERE user_id = $1', [userId]);
+        console.log('rows = ' + user);
+        if(user.rows.length < 1) return res.status(404).send('User not found.');
+        
+        db.query('DELETE FROM users WHERE user_id = $1', [userId]);
+        console.log(user);
+        //res.status(200).send(`User ${user.rows} deleted`);
+        const userJson = JSON.stringify(user);
+        console.log(userJson);
+        res.send(`${email} deleted`);
+    }catch(err){
+        res.status(500).send(err);
+    }
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
