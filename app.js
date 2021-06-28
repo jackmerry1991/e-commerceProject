ac0d44b605d9cca3a314a4181e6aff1a18a9325a
Jack - <jackmerry123@hotmail.com>
2021-06-28 17:56:29 +0100

    password encryption added

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 3000; 
 const db = require('./dbConnection.js')
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const e = require('express');
+const saltRounds = 10;
 //user routes move to users.js after this initial test
 
 
@@ -46,7 +49,7 @@ app.get('/user/:id', async (req, res) => {
 /**
  * Create new User
  */
-app.post('/user/create', async (req, res, next) => {
+app.post('/user/register', async (req, res, next) => {
     
     const userDetails = {
         email: email,
@@ -57,8 +60,14 @@ app.post('/user/create', async (req, res, next) => {
         password: password,
         postCode: postCode,
         } = req.body;
-    try{
-        const result = await db.query('INSERT INTO USERS(email, first_name, last_name, street, city, password, post_code) VALUES($1, $2, $3, $4, $5, $6, $7);', [email, firstName, lastName, street, city, password, postCode]);
+        if(!email || !password || !street || !city || !postCode || !firstName || !lastName) return res.status(400).send('Insufficient data');
+        const salt = await bcrypt.genSalt(10);
+        let encryptedPassword = await bcrypt.hash(password, salt);
+        console.log(encryptedPassword);
+        try{
+        const user = await db.query('SELECT * from users WHERE email = $1', [email]);
+        if(user.rows > 0) return res.status(200).send('User already exists');
+        const result = await db.query('INSERT INTO USERS(email, first_name, last_name, street, city, password, post_code) VALUES($1, $2, $3, $4, $5, $6, $7);', [email, firstName, lastName, street, city, encryptedPassword, postCode]);
         res.send(`User ${email} added successfully`);
     
     }catch(err){
@@ -70,6 +79,27 @@ app.post('/user/create', async (req, res, next) => {
 /**
  * Update existing user
  */
+app.put('/user', async (req, res) => {
+    const userId = req.body.userId;
+    const columnName = req.body.columnToUpdate;
+    const newValue = req.body.newValue;
+    console.log(userId);
+    console.log(columnName);
+    console.log(newValue);
+    
+    if(!userId) return res.status(400).send('Insufficient Data.');
+
+    try{
+        const user = await db.query('SELECT * FROM users WHERE user_id = $1', [userId]);
+        if(user.rows.length < 1) return res.status(400).send('User not found');
+        console.log('user found');
+        await db.query(`UPDATE users SET ${columnName} = $1 WHERE user_id = $2`, [newValue, userId]);
+        res.status(200).send(`User ${userId} successfully updated`);
+    }catch(err){
+        res.status(500).send('Error accessing DB');
+        console.log(err);
+    }
+});
 
 /**
  * Delete existing user
@@ -81,15 +111,11 @@ app.delete('/user/delete', async (req, res) => {
 
     try{
         const user = await db.query('SELECT * FROM users WHERE user_id = $1', [userId]);
-        console.log('rows = ' + user);
         if(user.rows.length < 1) return res.status(404).send('User not found.');
         
         db.query('DELETE FROM users WHERE user_id = $1', [userId]);
-        console.log(user);
-        //res.status(200).send(`User ${user.rows} deleted`);
-        const userJson = JSON.stringify(user);
-        console.log(userJson);
-        res.send(`${email} deleted`);
+        res.send(`${user.rows[0].email} deleted`);
+        //res.json(userJson);
     }catch(err){
         res.status(500).send(err);
     }
